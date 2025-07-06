ac77e2b68a35bc5ad23328305eaf27d25d26c67a
ivencomur <ivancortes@hotmail.com>
2025-07-06 03:13:31 -0400

    4.7: Continuous Delivery

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import * as atatus from 'atatus-spa';
+
+// --- Atatus Configuration ---
+
+atatus.config('b5c73e4a23fa47928d9bdce24bd2a7fe').install();
+
+
+atatus.notify(new Error('Test Atatus Setup'));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
